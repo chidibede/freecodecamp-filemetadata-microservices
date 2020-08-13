fdc108112dbb7666dae5d30cfbf2bce263fc4ba3
chidibede <chidibede@gmail.com>
2020-08-13 20:26:58 +0100

    completed the challenge

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
+var express = require("express");
+var cors = require("cors");
+const fileUpload = require("express-fileupload");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload({ createParentPath: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", function (req, res) {
+  if (!req.files) {
+    res.json({ message: "No file selected, please select a file" });
+  } else {
+    const file = req.files.upfile;
+    const fileInfo = {
+      name: file.name,
+      size: file.size
+    }
+    res.json(fileInfo);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server running on localhost:${port}`);
 });
